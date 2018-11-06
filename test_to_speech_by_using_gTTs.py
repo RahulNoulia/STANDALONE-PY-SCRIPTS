4ae515bbb2f6ae66e70da5640355a8e582c1e870
Rahu Noulia <36097160+RahulNoulia@users.noreply.github.com>
2018-11-06 10:22:20 +0530

    Update test_to_speech_by_using_gTTs.py

--- test_to_speech_by_using_gTTs.py
+++ test_to_speech_by_using_gTTs.py
@@ -1,9 +1,13 @@
 
 from gtts import *
 
-#  gTTs is a Google text=to-speech API which is very easy to use convert entered  text to voice (  by saving it in mp3 format )
+"""
 
-#  you can install gTTs API  by  typing this command ( pip install gTTS  )
+  gTTs is a Google text=to-speech API which is very easy to use and convert entered  text to voice 
+
+  you can install gTTs API  by  typing this command ( pip3 install gTTS  )
+
+"""
 
 import os
 
@@ -17,10 +21,12 @@ myobj = gTTS(text=mytext, lang=language, slow=True)   # here you can also give f
 
 myobj.save("myaudio.mp3")
 
-os.system("mpg321 myaudio.mp3")   # it save this mp3 file in your current directory where you execute your script
+os.system("mpg321 myaudio.mp3")   # it save this mp3 file in your current directory where you execute the script
 
 
+"""
+  if mpg321 is not installed than install it  by typing the command in  terminal ->  apt-get install mpg321
 
-#  if mpg321 is not installed than install it  by typing the command in  terminal ->  apt-get install mpg321
+  mpg321 is a very popular command-line mp3 player
 
-#  mpg321 is a very popular command-line mp3 player
+"""
